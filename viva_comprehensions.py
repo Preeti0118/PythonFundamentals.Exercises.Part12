8b62da44202a0a9d0cc825051a10b9a73e453666
Preeti Sehgal <preetisehgal2001@gmail.com>
2020-04-27 00:15:23 -0400

    defined conditional statements

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -10,26 +10,26 @@ class Parity(enum.Enum):
 def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     """
     return a list of even or odd numbers between start anbd stop argumnents 
-    depending on the argument parity. Return Even for parity 0 abnd return odd for parity 1
+    depending on the argument parity. Return Odd for parity 0 abnd return Even for parity 1
     :param start: The first int inclusive.
     :param stop: The last int exclusive.
     :param parity: specifying odd and even
     :return: list of numbers
     """
 
-    return [num for num in range(start, stop) if num % 2 != parity.value]
+    return [i for i in range(start, stop) if i % 2 != parity.value]
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
     """
-    accept argunenbts start and stop and return the value
+    accept arguments start and stop and return the value
     based on the function given in argument "strategy"
     :param start: starting integer in a dictionary (inclusive)
     :param stop: ending  integer in a dictionary (exclusive)
     :param strategy:A function to manipulate each digit
     :return: a dictionary integer
     """
-    return {num: strategy(num) for num in range(start, stop)}
+    return {i: strategy(i) for i in range(start, stop)}
 
 
 def gen_set(val_in: str) -> Set:
@@ -39,3 +39,6 @@ def gen_set(val_in: str) -> Set:
     :return: upper case list
     """
     return {word.upper() for word in val_in if word == word.lower()}
+
+
+#
